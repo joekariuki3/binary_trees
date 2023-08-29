3e2976d18146d6ac84430b1e13f9dacac41d5089
joekariuki3 <joekariuki3@gmail.com>
2023-08-29 12:48:41 +0300

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,10 +7,27 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t count_left = 0;
+	size_t count_right = 0;
+
 	if (tree == NULL)
 		return (0);
-	size_t count_left = binary_tree_height(tree->left);
-	size_t count_right = binary_tree_height(tree->right);
+	/* calculate length of left subtree if present else length is 0*/
+	if (tree->left != NULL)
+	{
+		count_left = 1 + binary_tree_height(tree->left);
+	}
+	else
+		count_left = 0;
 
-	return ((count_left > count_right ? count_left : count_right) + 1);
+	/* calculate length of right subtree if present else length is 0*/
+	if (tree->right != NULL)
+	{
+		count_right = 1 + binary_tree_height(tree->right);
+	}
+	else
+		count_right = 0;
+
+	/* return the largest length either the left one or the right one*/
+	return (count_left > count_right ? count_left : count_right);
 }
